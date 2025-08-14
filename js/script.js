32f3842b8c97c93364531189300540b61334ff8d
Sabiha Haque Chowdhury <sabihahaquechowdhury@gmail.com>
2025-08-14 20:00:25 +0600

    change

--- js/script.js
+++ js/script.js
@@ -141,13 +141,17 @@ document.addEventListener("DOMContentLoaded", () => {
     });
   }
 });
-const hamburger = document.getElementById("hamburger");
-const navMenu = document.getElementById("nav-menu");
-if (hamburger && navMenu) {
-  hamburger.addEventListener("click", () => {
-    navMenu.classList.toggle("show");
-  });
-}
+document.addEventListener("DOMContentLoaded", () => {
+  const hamburger = document.getElementById("hamburger");
+  const navMenu = document.getElementById("nav-menu");
+
+  if (hamburger && navMenu) {
+    hamburger.addEventListener("click", () => {
+      navMenu.classList.toggle("show");
+    });
+  }
+});
+
 
 
 
